e25834ab7388e75fea42e2912a07533b84bef680
BegadMousa <bwm7640@g.rit.edu>
2022-10-02 17:58:19 +0400

    init
    added files

---
+++ checkers.py
@@ -0,0 +1,82 @@
+'''
+This File is Part of the Python Turtle assignment 2 for the course: GCIS 123 section 600
+this file is checkers.py which is the first requirment for the assignment
+'''
+
+import turtle
+
+# constants
+PIXEL_SIZE = 30
+ROW_AND_COL = 20 # number of rows and columns, this is to be used in the Draw_checkers function becouse its is a 20x20 checker board
+ROW=20
+COLUMNS=20
+Start_CORD_x = -300
+Start_CORD_y = 250
+
+# Config
+def init():
+    turtle.speed(0)
+    turtle.up()
+    turtle.goto(-200,200)
+    turtle.pencolor("black")
+    turtle.pensize(1)
+
+# Draw Pixel
+def Draw_pixel(color):
+    i=0
+
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+
+    turtle.down()
+
+    # draw Pixel
+    while i<4:
+        turtle.forward(PIXEL_SIZE)
+        turtle.right(90)
+        i += 1
+
+    turtle.up()
+    turtle.end_fill()
+    turtle.forward(PIXEL_SIZE)
+
+# Draws pixels with alternating colors
+def Draw_alt_row(row,col,num_pixels,color="red",alt_color="black"):
+    # MOVe to start of row
+    xcor = Start_CORD_x
+    ycor = Start_CORD_y
+
+    # move to wanted row/col
+    xcor += PIXEL_SIZE*col
+    ycor -= PIXEL_SIZE*row
+
+    turtle.goto(xcor,ycor)
+    turtle.fillcolor(color)
+    
+    i=0
+    # draw row of pixels
+    for i in range(num_pixels):
+        if i%2==0: # alternate colors based iteration
+            Draw_pixel(color)
+        else:
+            Draw_pixel(alt_color)
+
+# draws a checker board using Draw_alt_row() function 
+def Draw_checkers(num_pixels,color="red",alt_color="black"):  
+    i=0
+    j=0
+ 
+    while j < num_pixels:
+        if j%2==0:
+            Draw_alt_row(j,0,num_pixels,color,alt_color)
+        else:
+            Draw_alt_row(j,0,num_pixels,alt_color,color)
+        j += 1
+
+def main():
+    init()
+    Draw_checkers(ROW_AND_COL) # num_pixels, color, alt_color 
+    turtle.done()
+
+if __name__ == "__main__":
+    main()
---
+++ drawings_test.py
@@ -0,0 +1,110 @@
+'''
+This is the Test file for drawing_test.py
+This File is Part of the Python Turtle assignment 2 for the course: GCIS 123 section 600
+this file is drawings.py which is the second requirment for the assignment
+To use this file you must have the turtle module installed
+to use this file you must input the file name you want to draw and the file must be in the same directory as this file
+the file must be a text file and must be 20x20 or 25x25
+the code will draw the art based on the file totaly automaticly without havig to input the colors manually
+'''
+
+import turtle
+
+# constants
+PIXEL_SIZE = 30
+ROW=20
+COLUMNS=20
+Start_CORD_x = -300
+Start_CORD_y = 250
+
+# Colors dictionary
+colors = {'0':"black",'1':"white",'2':"red", '3':"yellow", '4':"orange", '5':"green", '6':"yellowgreen", '7':"sienna", '8':"tan", '9':"gray", 'A':"darkgray"}
+
+# Config
+def init():
+    turtle.speed(0)
+    turtle.up()
+    turtle.goto(Start_CORD_x,Start_CORD_y)
+    turtle.pencolor("black")
+    turtle.pensize(1)
+
+def test_heading_init():
+    assert turtle.heading() == 0
+
+def test_pencolor():
+    assert turtle.pencolor() == "black"
+
+def test_start_cord_x():
+    assert Start_CORD_x == -300
+
+def test_start_cord_y():
+    assert Start_CORD_y == 250
+
+# Draw Pixel
+def Draw_pixel(color):
+    i=0
+
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+
+    turtle.down()
+
+    # draw Pixel
+    while i<4:
+        turtle.forward(PIXEL_SIZE)
+        turtle.right(90)
+        i += 1
+
+    turtle.up()
+    turtle.end_fill()
+    turtle.forward(PIXEL_SIZE)
+
+# draws a row of pixels based on the input string
+def Draw_row(row,col,num_pixels,color_string):
+    # MOVe to start of row
+    xcor = Start_CORD_x
+    ycor = Start_CORD_y
+
+    # move to wanted row/col
+    xcor += PIXEL_SIZE*col
+    ycor -= PIXEL_SIZE*row
+
+    turtle.goto(xcor,ycor)
+    
+    i=0
+    # draw row of pixels
+    while i < (num_pixels):
+        # print(colors[color_string[i]])
+        Draw_pixel(colors[color_string[i]])
+        i += 1
+
+# Draws the art based on the input file and the Draw_row function
+def Draw_art():
+    i=0
+    num_pixels = 20
+
+    # input file
+    filename = input("please enter to file name to draw: ")
+
+    # if no file set defualt to "drawing01.txt"
+    if filename == "":
+        filename = "drawing01.txt"
+
+    # draw art from file
+    with open(filename) as f:
+        for line in f:
+            # print(len(line))
+            if len(line) >= 25:
+                num_pixels = 25
+                
+            Draw_row(i,0,num_pixels,line)
+            i += 1
+
+def main():
+    init()
+    Draw_art()
+    turtle.done()
+
+
+if __name__ == "__main__":
+    main()
